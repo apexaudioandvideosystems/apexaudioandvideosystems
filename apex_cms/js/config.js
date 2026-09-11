3fc59cd66609e662e5a4f682741e86fa4984a7b0
apexaudioandvideosystems <apexaudioandvideosystems@gmail.com>
2026-09-11 16:40:44 -0500

    Add files via upload

---
+++ apex_cms/js/config.js
@@ -0,0 +1,5 @@
+// Add your Supabase project values here.
+window.APEX_CONFIG = {
+  supabaseUrl: 'https://lexzgzmjugfdvqapyxav.supabase.co',
+  supabaseAnonKey: 'sb_publishable_0Q9BDbrZenQqZCTozIU-kA_G6SvjGGH'
+};
